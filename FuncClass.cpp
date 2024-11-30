6ff642a1ee20cbba6d718bba982d248ac948cbca
valerababenko <valerii.babenko@renesandro.ai>
2024-11-30 23:20:03 +0200

    hotfix change func sum of first 3 items

--- FuncClass.cpp
+++ FuncClass.cpp
@@ -1,5 +1,11 @@
 ﻿#include "FuncClass.h"
 
 double FuncClass::FuncA(double x) {
-    return 0.0;
+    double sum = 0.0;
+    for (int i = 0; i < 3; ++i) {
+        double numerator = std::pow(-1, i) * factorial(2 * i);
+        double denominator = std::pow(4, i) * std::pow(factorial(i), 2);
+        sum += (numerator / denominator) * std::pow(x, i);
+    }
+    return sum;
 }
