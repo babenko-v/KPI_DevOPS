722cada011c8c9e640ff8771c747f452102fa70f
babenko-v <1111valerababenko@gmail.com>
2024-12-15 16:02:04 +0200

    add nginx

--- main.cpp
+++ main.cpp
@@ -1,8 +1,44 @@
 ﻿#include <iostream>
+#include <sys/wait.h>
 #include "FuncClass.h"
 
 int CreateHTTPserver();
-int main() {
+
+void singChldHandler(int s) 
+{
+	printf("Caught signal SIGCHILD\n");
+
+	pid_t pid;
+	int status;
+
+	while ((pid = waitpid(-1, &status,WNOHANG))>0)
+	{
+		if (WIFEXITED(status)) printf("\nChild process terminated");
+	}
+}
+
+void singInthandler(int s) 
+{
+	printf("Caught signal %d. Starting graceful exit procedure\n",s);
+
+	pid_t pid;
+	int status;
+	while ((pid = waitpid(-1,&status,0))>0)
+	{
+		if(WIFEXITED(status)) printf("\nChild process terminated");
+
+	}
+
+	if (pid==-1) printf("\nAll child processes terminated");
+
+	exit(EXIT_SUCCESS);
+}
+
+int main(int argc, char* argv[]) {
+
+    signal(SIGCHLD, singChldHandler);
+    signal(SIGINT, singInthandler);
+
     FuncClass obj;
     std::cout << "FuncA result: " << obj.FuncA(5.0, 1.0) << std::endl;
 
